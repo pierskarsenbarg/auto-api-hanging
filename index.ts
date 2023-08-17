2fdbc4da7f7390dabe38f169834623acfd69b115
Piers Karsenbarg <piers@pulumi.com>
2023-08-17 08:47:10 +0100

    Code submitted by customer

--- index.ts
+++ index.ts
@@ -1,46 +1,43 @@
-import { LocalWorkspace } from "@pulumi/pulumi/automation";
-import * as aws from "@pulumi/aws";
+import { LocalWorkspace } from '@pulumi/pulumi/automation';
+import * as aws from '@pulumi/aws';
 import * as pulumi from "@pulumi/pulumi";
-import * as apigateway from "@pulumi/aws-apigateway";
+import * as apigateway from '@pulumi/aws-apigateway';
 
-const projectName = "apigw-test";
+const projectName = 'apigw-test';
 (async () => {
   const pulumiProgram = async () => {
     const role = new aws.iam.Role(`${projectName}-iam-role`, {
-      assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
-        aws.iam.Principals.LambdaPrincipal
-      ),
+      assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(aws.iam.Principals.LambdaPrincipal),
       managedPolicyArns: [aws.iam.ManagedPolicies.AWSLambdaBasicExecutionRole],
     });
-
+  
+    const code = new pulumi.asset.AssetArchive({
+      ".": new pulumi.asset.FileArchive("./app")
+    });
+  
     // Create a Lambda function
     const lambdaFunction = new aws.lambda.Function(`${projectName}-lambda`, {
-      code: new pulumi.asset.AssetArchive({
-        ".": new pulumi.asset.FileArchive("./app"),
-      }),
+      code,
       role: role.arn,
-      handler: "getCustomerHandler.handler",
-      runtime: "nodejs18.x",
+      handler: 'getCustomerHandler.handler',
+      runtime: 'nodejs18.x',
     });
-
+  
     // Create a API Gateway Rest API
-    // const restAPI = new apigateway.RestAPI(`${projectName}-api-gateway`, {
-    //   routes: [
-    //     {
-    //       method: "GET",
-    //       path: "/getCustomer",
-    //       eventHandler: lambdaFunction,
-    //     },
-    //   ],
-    // });
+    const restAPI = new apigateway.RestAPI(`${projectName}-api-gateway`, {
+      routes: [
+        {
+          method: 'GET',
+          path: '/getCustomer',
+          eventHandler: lambdaFunction,
+        },
+      ],
+      // stageName: 'dev',
+    });
   };
 
-  const stack = await LocalWorkspace.createOrSelectStack({
-    program: pulumiProgram,
-    projectName,
-    stackName: "test",
-  });
-  await stack.setConfig("aws:region", { value: "eu-west-2" });
-  await stack.up({ onOutput: console.log, logVerbosity: 10, logToStdErr: true, });
-//   await stack.destroy({ onOutput: console.log });
+  const stack = await LocalWorkspace.createOrSelectStack({ program: pulumiProgram, projectName, stackName: 'test' });
+  await stack.setConfig('aws:region', { value: 'eu-west-2' });
+  await stack.up({ onOutput: console.log, logVerbosity: 10, logToStdErr: true });
+  // await stack.destroy({ onOutput: console.log });
 })();
